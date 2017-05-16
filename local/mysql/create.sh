a7b7df213a5dffb888fb6dad304096be6f86d0b4
Glenn Renfro <grenfro@pivotal.io>
2017-05-16 10:11:41 -0400

    Renamed  SPRING_DATASOURCE_SCHEMA to SPRING_SCHEMA_NAME

--- local/mysql/create.sh
+++ local/mysql/create.sh
@@ -7,7 +7,7 @@ mysql:
   image: mysql:5.7
   environment:
     MYSQL_ROOT_PASSWORD: $SPRING_DATASOURCE_PASSWORD
-    MYSQL_DATABASE: $SPRING_DATASOURCE_SCHEMA
+    MYSQL_DATABASE: $SPRING_SCHEMA_NAME
     MYSQL_USER: $SPRING_DATASOURCE_USERNAME
     MYSQL_PASSWORD: $SPRING_DATASOURCE_PASSWORD
   ports:
@@ -16,4 +16,4 @@ EOF
 
 create "mysql" 3306
 echo "Service host is $SERVICE_HOST"
-APPLICATION_ARGS="$APPLICATION_ARGS --spring.datasource.driver-class-name=org.mariadb.jdbc.Driver --spring.datasource.url=jdbc:mysql://$SERVICE_HOST/$SPRING_DATASOURCE_SCHEMA --spring.datasource.username=$SPRING_DATASOURCE_USERNAME --spring.datasource.password=$SPRING_DATASOURCE_PASSWORD"
+APPLICATION_ARGS="$APPLICATION_ARGS --spring.datasource.driver-class-name=org.mariadb.jdbc.Driver --spring.datasource.url=jdbc:mysql://$SERVICE_HOST/$SPRING_SCHEMA_NAME --spring.datasource.username=$SPRING_DATASOURCE_USERNAME --spring.datasource.password=$SPRING_DATASOURCE_PASSWORD"
