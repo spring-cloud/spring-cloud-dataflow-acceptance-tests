2780f0ae51a17c8bbdf5ce42eef2badde547aee4
Thomas Risberg <trisberg@pivotal.io>
2017-05-16 15:57:06 -0400

    Updating docker-compose error message

--- local/common.sh
+++ local/common.sh
@@ -19,7 +19,7 @@ function deploy_docker_container() {
 
   [ -z "$DOCKER_SERVER" ] && { echo "Environment variable DOCKER_SERVER must be set"; exit 1; }
   if ! command_exists docker-compose; then
-    echo "It appears that you don't have a docker command line executable available. Halting."
+    echo "It appears that you don't have a docker-compose command line executable available. Halting."
     exit 1
   fi
 
