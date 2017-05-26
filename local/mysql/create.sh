1dbe0b8d063eca18c159701a4ec293c93a579c24
Mark Pollack <mpollack@pivotal.io>
2017-05-26 16:44:26 -0400

    Add run.sh option to avoid downloading the server
    * Note had to add a `sleep 5` command to wait until mysql was really read to serve requests vs. the netcat port test.
    
    Fixes #92

--- local/mysql/create.sh
+++ local/mysql/create.sh
@@ -15,5 +15,6 @@ mysql:
 EOF
 
 create "mysql" 3306
+sleep 5
 echo "Service host is $SERVICE_HOST"
 APPLICATION_ARGS="$APPLICATION_ARGS --spring.datasource.driver-class-name=org.mariadb.jdbc.Driver --spring.datasource.url=jdbc:mysql://$SERVICE_HOST/$SPRING_SCHEMA_NAME --spring.datasource.username=$SPRING_DATASOURCE_USERNAME --spring.datasource.password=$SPRING_DATASOURCE_PASSWORD"
