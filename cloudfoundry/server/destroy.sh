156644782bed7c72ae07630a076d8019bac37aa4
Ilayaperumal Gopinathan <igopinathan@pivotal.io>
2017-07-26 11:19:35 +0530

    Cleanup scdf server only if exists
    Resolves #118
    
    Delete the apps as part of server removal

--- cloudfoundry/server/destroy.sh
+++ cloudfoundry/server/destroy.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-cf delete scdf-server -f -r
+ if (cf apps | grep "scdf-server"); then
+    cf delete scdf-server -f -r
+ fi
+
+ apps=`cf apps | awk 'FNR > 3 {print $1}'`
+ for app in $apps
+ do
+    if [[ $app != 'No' ]]; then
+        echo "Deleting the app: $app"
+        cf delete $app -f -r
+    fi
+ done
+
