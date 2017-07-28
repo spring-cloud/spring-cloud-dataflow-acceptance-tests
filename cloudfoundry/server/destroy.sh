21f72141a1ec813af80e021c2a1e30d60e026c37
Ilayaperumal Gopinathan <igopinathan@pivotal.io>
2017-07-28 11:59:34 +0530

    Delete the apps as part of server removal

--- cloudfoundry/server/destroy.sh
+++ cloudfoundry/server/destroy.sh
@@ -3,3 +3,12 @@
  if (cf apps | grep "scdf-server"); then
     cf delete scdf-server -f -r
  fi
+
+ apps=`cf apps | awk 'FNR > 3 {print $1}'`
+ for app in $apps
+ do
+    if [[ $app != 'No' ]]; then
+        echo "Deleting the app: $app"
+        cf delete $app -f -r
+    fi
+ done
