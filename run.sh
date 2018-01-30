3f8f0b0348478e86b909535f3bd0f99d45315187
Ilayaperumal Gopinathan <igopinathan@pivotal.io>
2018-01-30 21:35:55 +0530

    Remove skipper server check from top level run.sh

--- run.sh
+++ run.sh
@@ -120,8 +120,6 @@ function setup() {
     if [  ! -z "$skipperMode" ]; then
       run_scripts "skipper-server" "create.sh"
       export SPRING_CLOUD_DATAFLOW_FEATURES_SKIPPER_ENABLED=true
-      SKIPPER_SERVER_URI=$(kubectl get svc skipper --namespace $KUBERNETES_NAMESPACE | grep skipper | awk '{print $4}')
-      export SKIPPER_SERVER_URI="http://$SKIPPER_SERVER_URI:7577"
     fi
     run_scripts "server" "create.sh"
   popd
