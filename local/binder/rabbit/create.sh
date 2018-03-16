973e7b7d3f1447b82b2a93143146800741c4f7a5
Mark Pollack <mpollack@pivotal.io>
2018-03-16 17:52:22 -0400

    Stop docker containers before running tests

--- local/binder/rabbit/create.sh
+++ local/binder/rabbit/create.sh
@@ -2,5 +2,8 @@
 
 source ../../common.sh
 
+echo "cleaning up any previous rabbit docker containers..."
+docker ps -q --filter ancestor="rabbitmq:management" | xargs -r docker stop
+echo "done cleaning up."
 create "rabbitmq" 5672
 run_scripts "$PWD" "config.sh"
--- local/redis/create.sh
+++ local/redis/create.sh
@@ -2,6 +2,9 @@
 
 source ../common.sh
 
+echo "cleaning up any previous redis docker containers..."
+docker ps -q --filter ancestor="redis" | xargs -r docker stop
+echo "done cleaning up."
 create "redis" 6379
 
 APPLICATION_ARGS="$APPLICATION_ARGS  --spring.redis.host=$SERVICE_HOST"
