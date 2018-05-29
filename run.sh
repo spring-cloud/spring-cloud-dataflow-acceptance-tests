7f434ab2d0c77045f2cc3b3d37b4e2b029591ea9
Mark Pollack <mpollack@pivotal.io>
2018-05-29 12:34:26 -0400

    Update default to skipper 1.0.5.BUILD-SNAPSHOT

--- run.sh
+++ run.sh
@@ -292,7 +292,7 @@ done
 # ======================================= DEFAULTS ============================================
 [[ -z "${PLATFORM}" ]] && PLATFORM=local
 [[ -z "${BINDER}" ]] && BINDER=rabbit
-[[ -z "${SKIPPER_VERSION}" ]] && SKIPPER_VERSION=1.0.4.RELEASE
+[[ -z "${SKIPPER_VERSION}" ]] && SKIPPER_VERSION=1.0.5.BUILD-SNAPSHOT
 [[ -z "${DATAFLOW_VERSION}" ]] && DATAFLOW_VERSION=1.5.1.BUILD-SNAPSHOT
 [[ -z "${STREAM_APPS_VERSION}" ]] && STREAM_APPS_VERSION=Celsius.SR2
 [[ -z "${TASKS_VERSION}" ]] && TASKS_VERSION=Clark.RELEASE
