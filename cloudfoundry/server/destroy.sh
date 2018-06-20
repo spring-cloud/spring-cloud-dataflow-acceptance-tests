66840312d9f61d67a5658691ea4d91842ecc1360
Mark Pollack <mpollack@pivotal.io>
2018-06-20 18:09:14 -0400

    add guard to cf delete command

--- cloudfoundry/server/destroy.sh
+++ cloudfoundry/server/destroy.sh
@@ -2,8 +2,10 @@
 
  if (cf apps | grep "scdf-server"); then
     SCDF_NAME=$(cf apps | grep dataflow-server- | awk '{print $1}' | sed 's:,::g')
-    echo "Deleting: $SCDF_NAME"
-    cf delete $SCDF_NAME -f -r
+    if [[ ! -z $SCDF_NAME ]]; then
+      echo "Deleting: $SCDF_NAME"
+      cf delete $SCDF_NAME -f -r
+    fi
  fi
 
  apps=`cf apps | awk 'FNR > 3 {print $1}'`
