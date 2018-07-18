5129de70c3ef70c0252cb0db3e2e19bd4ae6471b
Mark Pollack <mpollack@pivotal.io>
2018-07-18 15:00:24 -0400

    Fix determination of skipper_server_uri on PCF

--- cloudfoundry/skipper-server/config.sh
+++ cloudfoundry/skipper-server/config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function config_application() {
-  SKIPPER_SERVER_URI=$(cf app skipper-server | grep skipper-server- | awk '{print $2}' | sed 's:,::g')
+  SKIPPER_SERVER_URI=$(cf apps | grep skipper-server- | awk '{print $6}' | sed 's:,::g')
   SKIPPER_SERVER_URI="http://$SKIPPER_SERVER_URI"
   echo "SKIPPER SERVER URI: $SKIPPER_SERVER_URI"
   export SKIPPER_SERVER_URI
