608a0fd4f938b57f792258720eee839af9edfc9a
Glenn Renfro <grenfro@pivotal.io>
2018-07-31 08:29:23 -0400

    Add Scheduler Acceptance Tests
    2 Notes:
    * Tests should skip on current AT's.  Because scheduling is disabled.
    * Merge only after https://github.com/spring-cloud/spring-cloud-dataflow/pull/2342 has been merged.
    
    resolves #184

---
+++ src/test/java/org/springframework/cloud/dataflow/acceptance/test/util/SchedulerRule.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.dataflow.acceptance.test.util;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.dataflow.rest.client.DataFlowTemplate;
+import org.springframework.cloud.dataflow.rest.client.SchedulerOperations;
+import org.springframework.cloud.scheduler.spi.junit.AbstractExternalResourceTestSupport;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.Assert;
+
+/**
+ * Verifies that the Spring Cloud Data Flow Scheduler is available.
+ *
+ * @author Glenn Renfro
+ */
+public class SchedulerRule extends AbstractExternalResourceTestSupport<DataFlowTemplate> {
+
+	private ConfigurableApplicationContext context;
+
+	public SchedulerRule() {
+		super("SCHEDULER");
+	}
+
+	@Override
+	protected void cleanupResource() throws Exception {
+		context.close();
+	}
+
+	@Override
+	protected void obtainResource() throws Exception {
+		context = new SpringApplicationBuilder(Config.class).web(false).run();
+		resource = context.getBean(DataFlowTemplate.class);
+
+		SchedulerOperations schedulerOperations = resource.schedulerOperations();
+		Assert.notNull(schedulerOperations, "no SchedulerOperations bean is available.  Skipping test.");
+	}
+
+	@Configuration
+	@EnableConfigurationProperties
+	public static class Config {
+
+		@Bean
+		@ConfigurationProperties
+		public TestConfigurationProperties cloudFoundryConnectionProperties() {
+			return new TestConfigurationProperties();
+		}
+
+		@Bean
+		public DataFlowTemplate dataFlowTemplate(TestConfigurationProperties configurationProperties) {
+			DataFlowTemplate dataFlowOperationsTemplate = null;
+			try {
+				dataFlowOperationsTemplate = new DataFlowTemplate(
+						new URI(configurationProperties.getServerUri()));
+			}
+			catch (URISyntaxException uriException) {
+				throw new IllegalStateException(uriException);
+			}
+			return dataFlowOperationsTemplate;
+		}
+
+	}
+}
