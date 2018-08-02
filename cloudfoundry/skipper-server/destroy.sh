ada6620983cd6f969fdf74e076f6a0225ba52a99
Mark Pollack <mpollack@pivotal.io>
2018-08-02 11:31:55 -0400

    Delete multiple skipper servers on pcf

--- cloudfoundry/skipper-server/destroy.sh
+++ cloudfoundry/skipper-server/destroy.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
  if (cf apps | grep "skipper-server"); then
-    echo "Deleting the skipper server"
-    cf delete skipper-server -f -r
+    SKIPPER_NAME=$(cf apps | grep skipper-server- | awk '{print $1}' | sed 's:,::g')
+    for server in $SKIPPER_NAME
+    do
+      if [[ $server != 'No' ]]; then
+        echo "Deleting the app: $server"
+        cf delete $server -f -r
+      fi
+    done
  fi
 
  apps=`cf apps | awk 'FNR > 3 {print $1}'`
