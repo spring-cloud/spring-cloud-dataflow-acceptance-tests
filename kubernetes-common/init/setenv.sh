970c0821dc579b3e72ae0d957e1a266fde29ee63
Ilayaperumal Gopinathan <igopinathan@pivotal.io>
2018-12-19 18:57:13 +0530

    Update SPRING_APPLICATION_JSON for k8s env

--- kubernetes-common/init/setenv.sh
+++ kubernetes-common/init/setenv.sh
@@ -5,7 +5,7 @@ if [ -z "$DATAFLOW_SERVER_NAME" ]; then
     DATAFLOW_SERVER_NAME="spring-cloud-dataflow-server-kubernetes"
 fi
 if [ -z "$SPRING_APPLICATION_JSON" ]; then
-    SPRING_APPLICATION_JSON="{ \"maven\": { \"local-repository\": null, \"remote-repositories\": { \"repo1\": { \"url\": \"https://repo.spring.io/libs-snapshot\"} } }, \"spring.cloud.dataflow.application-properties.stream.spring.cloud.stream.bindings.applicationMetrics.destination\": \"metrics\" }"
+    SPRING_APPLICATION_JSON="{ \"maven\": { \"local-repository\": null, \"remote-repositories\": { \"repo1\": { \"url\": \"https://repo.spring.io/libs-snapshot\"} } }, \"spring.cloud.dataflow.application-properties.stream.spring.cloud.stream.bindings.applicationMetrics.destination\": \"metrics\", \"spring.cloud.dataflow.task.platform.kubernetes.accounts.cluster1.memory\" : \"1024Mi\",\"spring.cloud.dataflow.task.platform.kubernetes.accounts.cluster1.createDeployment\" : true }"
 fi
 exit 1
 
