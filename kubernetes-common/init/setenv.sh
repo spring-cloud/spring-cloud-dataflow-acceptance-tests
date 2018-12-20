b7a7ef94b07ede39ade3894b18d42965286d92c0
Chris Schaefer <cschaefer@pivotal.io>
2018-12-20 01:01:14 -0500

    move setting of SAJ into kubernetes-common/server/create.sh - kubernetes-common/init/setenv.sh does not get evaluated, only in specific platforms

--- kubernetes-common/init/setenv.sh
+++ kubernetes-common/init/setenv.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
 echo "kubernetes-common contains shared files and not intended to be used as its own target"
-if [ -z "$DATAFLOW_SERVER_NAME" ]; then
-    DATAFLOW_SERVER_NAME="spring-cloud-dataflow-server-kubernetes"
-fi
-if [ -z "$SPRING_APPLICATION_JSON" ]; then
-    SPRING_APPLICATION_JSON="{ \"maven\": { \"local-repository\": null, \"remote-repositories\": { \"repo1\": { \"url\": \"https://repo.spring.io/libs-snapshot\"} } }, \"spring.cloud.dataflow.application-properties.stream.spring.cloud.stream.bindings.applicationMetrics.destination\": \"metrics\", \"spring.cloud.dataflow.task.platform.kubernetes.accounts.cluster1.memory\" : \"1024Mi\",\"spring.cloud.dataflow.task.platform.kubernetes.accounts.cluster1.createDeployment\" : true }"
-fi
+
 exit 1
 
--- kubernetes-common/server/create.sh
+++ kubernetes-common/server/create.sh
@@ -92,6 +92,14 @@ EOF
 
 }
 
+if [ -z "$DATAFLOW_SERVER_NAME" ]; then
+    DATAFLOW_SERVER_NAME="spring-cloud-dataflow-server-kubernetes"
+fi
+
+if [ -z "$SPRING_APPLICATION_JSON" ]; then
+    SPRING_APPLICATION_JSON="{ \"maven\": { \"local-repository\": null, \"remote-repositories\": { \"repo1\": { \"url\": \"https://repo.spring.io/libs-snapshot\"} } }, \"spring.cloud.dataflow.application-properties.stream.spring.cloud.stream.bindings.applicationMetrics.destination\": \"metrics\", \"spring.cloud.dataflow.task.platform.kubernetes.accounts.cluster1.memory\" : \"1024Mi\",\"spring.cloud.dataflow.task.platform.kubernetes.accounts.cluster1.createDeployment\" : true }"
+fi
+
 RETRIES=20
 WAIT_TIME=15
 generate_manifest
