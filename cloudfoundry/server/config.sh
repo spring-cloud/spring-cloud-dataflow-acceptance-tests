fc9869048901d167b0dbad8890c9429c94549e71
Ilayaperumal Gopinathan <igopinathan@pivotal.io>
2019-06-19 11:24:58 +0530

    Update CF/K8s http scheme

--- cloudfoundry/server/config.sh
+++ cloudfoundry/server/config.sh
@@ -2,7 +2,7 @@
 
 function config_application() {
   SERVER_URI=$(cf apps | grep dataflow-server- | awk '{print $6}' | sed 's:,::g')
-  SERVER_URI="http://$SERVER_URI"
+  SERVER_URI="https://$SERVER_URI"
   echo "SCDF SERVER URI: $SERVER_URI"
   export SERVER_URI
   export PLATFORM_SUFFIX=$SPRING_CLOUD_DEPLOYER_CLOUDFOUNDRY_DOMAIN
--- cloudfoundry/skipper-server/config.sh
+++ cloudfoundry/skipper-server/config.sh
@@ -2,7 +2,7 @@
 
 function config_application() {
   SKIPPER_SERVER_URI=$(cf apps | grep skipper-server- | awk '{print $6}' | sed 's:,::g')
-  SKIPPER_SERVER_URI="http://$SKIPPER_SERVER_URI"
+  SKIPPER_SERVER_URI="https://$SKIPPER_SERVER_URI"
   echo "SKIPPER SERVER URI: $SKIPPER_SERVER_URI"
   export SKIPPER_SERVER_URI
 }
