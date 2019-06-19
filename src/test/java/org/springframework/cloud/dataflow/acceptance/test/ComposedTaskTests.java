78e7df2279b0bac616a54f8ca08cf73531cf339d
Ilayaperumal Gopinathan <igopinathan@pivotal.io>
2019-06-19 13:13:52 +0530

    Fix checkstyle error

--- src/test/java/org/springframework/cloud/dataflow/acceptance/test/ComposedTaskTests.java
+++ src/test/java/org/springframework/cloud/dataflow/acceptance/test/ComposedTaskTests.java
@@ -18,9 +18,7 @@ package org.springframework.cloud.dataflow.acceptance.test;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.junit.Test;
 
