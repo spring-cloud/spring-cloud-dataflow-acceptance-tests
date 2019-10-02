99d8563370d14f7a1a035b9233dcc333fc0254cd
Chris Schaefer <cschaefer@pivotal.io>
2019-10-02 17:01:28 -0400

    Cleanup distro file installed objects after clean run

--- kubernetes-common/skipper-server/destroy.sh
+++ kubernetes-common/skipper-server/destroy.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
 
+function distro_files_object_delete() {
+  kubectl delete all -l app=rabbitmq --namespace $KUBERNETES_NAMESPACE || true
+  kubectl delete all,pvc,secrets -l app=mysql --namespace $KUBERNETES_NAMESPACE || true
+  kubectl delete all,cm -l app=skipper --namespace $KUBERNETES_NAMESPACE || true
+  kubectl delete all -l app=kafka --namespace $KUBERNETES_NAMESPACE || true
+  kubectl delete all,cm -l app=scdf-server --namespace $KUBERNETES_NAMESPACE || true
+  kubectl delete role scdf-role --namespace $KUBERNETES_NAMESPACE || true
+  kubectl delete rolebinding scdf-rb --namespace $KUBERNETES_NAMESPACE || true
+  kubectl delete serviceaccount scdf-sa --namespace $KUBERNETES_NAMESPACE || true
+}
+
 if [ -z "$USE_DISTRO_FILES" ]; then
   helm delete scdf --purge || true
+else
+  distro_files_object_delete
 fi
+
