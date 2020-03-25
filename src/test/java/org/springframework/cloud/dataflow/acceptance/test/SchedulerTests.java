853ffdb5b6c6fde4a2fb2dda14516e626bf816bf
Glenn Renfro <grenfro@pivotal.io>
2020-03-25 14:45:04 -0400

    Updated the tests so that the suffix for schedule names has been removed

--- src/test/java/org/springframework/cloud/dataflow/acceptance/test/SchedulerTests.java
+++ src/test/java/org/springframework/cloud/dataflow/acceptance/test/SchedulerTests.java
@@ -83,7 +83,7 @@ public class SchedulerTests extends AbstractTaskTests {
 		String[] scheduleNames = new String[ENTRY_COUNT];
 		for(int i = 0; i < ENTRY_COUNT; i++) {
 			taskNames[i] = taskCreate("timestamp");
-            scheduleNames[i] = createDefaultSchedule(taskNames[i]) + "-" + "scdf-" + taskNames[i];
+            scheduleNames[i] = createDefaultSchedule(taskNames[i]);
 		}
 		for(int i = 0; i < ENTRY_COUNT; i++) {
 			PagedResources<ScheduleInfoResource> pagedResources = listSchedules(taskNames[i]);
@@ -101,7 +101,7 @@ public class SchedulerTests extends AbstractTaskTests {
 
 	private String createDefaultSchedule() {
 		String taskDefinitionName = taskCreate("timestamp");
-		return  createDefaultSchedule(taskDefinitionName) + "-" +"scdf-" + taskDefinitionName ;
+		return  createDefaultSchedule(taskDefinitionName);
 	}
 
 	private String createDefaultSchedule(String taskName) {
