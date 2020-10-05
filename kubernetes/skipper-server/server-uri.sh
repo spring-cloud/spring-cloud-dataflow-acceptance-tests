ba2a1ea9a9edeede9ffd66d3fe5e53d9b2c8c378
Chris Schaefer <cschaefer@vmware.com>
2020-10-05 14:21:19 -0400

    fix skipper host ip regex

--- kubernetes/skipper-server/server-uri.sh
+++ kubernetes/skipper-server/server-uri.sh
@@ -1,7 +1,7 @@
 if [ -z "$SKIPPER_SERVER_URI" ] ;
 then
   SKIPPER_IP=$(kubectl get svc --namespace $KUBERNETES_NAMESPACE | grep skipper | awk '{print $4}')
-  if [[ "$SKIPPER_IP" =~ [\d2:\d2:\d3:\d+] ]] ;
+  if [[ "$SKIPPER_IP" =~ [\d{1-3}:\d{1-3}:\d{1-3}:\d{1-3}] ]] ;
   then
        export SKIPPER_SERVER_URI="http://$SKIPPER_IP"
        wait_for_200 ${SKIPPER_SERVER_URI}/api/about
