90cc718689df2bf387793dbde5349383d2882341
Janne Valkealahti <janne.valkealahti@gmail.com>
2020-11-30 11:55:17 +0000

    Add split test
    - Relates #324

--- src/test/java/org/springframework/cloud/dataflow/acceptance/test/ComposedTaskTests.java
+++ src/test/java/org/springframework/cloud/dataflow/acceptance/test/ComposedTaskTests.java
@@ -106,6 +106,40 @@ public class ComposedTaskTests extends AbstractTaskTests {
 		assertThat(taskExecutionResources).hasSize(0);
 	}
 
+	@Test
+	public void ctrSplit() {
+		// splitTest COMPLETE
+		// splitTest-t1 COMPLETE
+		// splitTest-t2 COMPLETE
+		// splitTest-t3 COMPLETE
+		//
+		// splitTest exit 0
+		// splitTest-t1 exit 0
+		// splitTest-t2 exit 0
+		// splitTest-t3 exit 0
+		String taskDefinitionName = composedTaskLaunch("<t1:timestamp || t2:timestamp || t3:timestamp>");
+		assertThat(waitForTaskToComplete(taskDefinitionName + "-t1", 1)).isTrue();
+		assertThat(waitForTaskToComplete(taskDefinitionName + "-t2", 1)).isTrue();
+		assertThat(waitForTaskToComplete(taskDefinitionName + "-t3", 1)).isTrue();
+		assertThat(waitForTaskToComplete(taskDefinitionName, 1)).isTrue();
+
+		List<TaskExecutionResource> taskExecutionResources = getTaskExecutionResource(taskDefinitionName);
+		assertThat(taskExecutionResources).hasSize(1);
+		assertThat(taskExecutionResources.get(0).getExitCode()).isEqualTo(0);
+
+		taskExecutionResources = getTaskExecutionResource(taskDefinitionName + "-t1");
+		assertThat(taskExecutionResources).hasSize(1);
+		assertThat(taskExecutionResources.get(0).getExitCode()).isEqualTo(0);
+
+		taskExecutionResources = getTaskExecutionResource(taskDefinitionName + "-t2");
+		assertThat(taskExecutionResources).hasSize(1);
+		assertThat(taskExecutionResources.get(0).getExitCode()).isEqualTo(0);
+
+		taskExecutionResources = getTaskExecutionResource(taskDefinitionName + "-t3");
+		assertThat(taskExecutionResources).hasSize(1);
+		assertThat(taskExecutionResources.get(0).getExitCode()).isEqualTo(0);
+	}
+
 	private void assertTaskExecutions(String taskDefinitionName,
 			int expectedExitCode, int expectedCount) {
 		assertTrue(waitForTaskToComplete(taskDefinitionName + "-a", expectedCount));
