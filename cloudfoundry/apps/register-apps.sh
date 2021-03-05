9a6fe948c953fc1bccfa8bda71d4acd0576ebe10
Christian Tzolov <christian.tzolov@gmail.com>
2021-03-05 12:50:55 +0100

    improve SERVER_URI resoltion for apps

--- cloudfoundry/apps/register-apps.sh
+++ cloudfoundry/apps/register-apps.sh
@@ -1,5 +1,5 @@
 SERVER_URI=$(cf apps | grep dataflow-server- | awk '{print $6}' | sed 's:,::g')
-SERVER_URI="https://$SERVER_URI"
+export SERVER_URI="https://$SERVER_URI"
 
 echo "SCDF SERVER URI: $SERVER_URI"
 
