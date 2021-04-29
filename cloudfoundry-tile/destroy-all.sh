e465be6d06678a5b848a0658177fa67d08becfc4
David Turanski <dturanski@vmware.com>
2021-04-29 09:56:24 -0400

    Dataflow tile: Clean up apps on cleanup servers

--- cloudfoundry-tile/destroy-all.sh
+++ cloudfoundry-tile/destroy-all.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
 source $ROOT_DIR/$PLATFORM/common.sh
-
-# Delete any remaing apps before deleting services since some may be bound to them.
-
-for app in $(cf apps | tail +5 | awk '{print $1}'); 
-do 
-        cf delete -f -r $app 
-done
-
 schedulesEnabled=$1
 cf delete-service-key -f $DATAFLOW_SERVICE_INSTANCE_NAME $DATAFLOW_SERVICE_KEY
 destroy_service $DATAFLOW_SERVICE_INSTANCE_NAME
--- cloudfoundry-tile/server/destroy.sh
+++ cloudfoundry-tile/server/destroy.sh
@@ -1,4 +1,8 @@
 if [ -f "${ROOT_DIR}/$CERT_URI.cer" ]; then
     rm -f "${ROOT_DIR}/$CERT_URI.cer"
 fi
-
+# Delete any remaing apps before deleting services since some may be bound to them.
+for app in $(cf apps | tail +5 | awk '{print $1}');
+do
+        cf delete -f -r $app
+done
