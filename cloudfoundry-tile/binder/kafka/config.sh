cd3923715837b39b1894bd2584df7ce0be5a8f9f
David Turanski <dturanski@vmware.com>
2021-04-30 09:06:19 -0400

    Automate tile kafka set up

--- cloudfoundry-tile/binder/kafka/config.sh
+++ cloudfoundry-tile/binder/kafka/config.sh
@@ -4,7 +4,7 @@
 [[ -z "$DATAFLOW_TILE_KAFKA_PASSWORD" ]] && { echo "$(basename $BASH_SOURCE) DATAFLOW_TILE_KAFKA_PASSWORD is required"; exit 1; }
 load_file "$PWD/env.properties"
 if [[ $STREAM_APPS_VERSION == *"latest"* ]]; then
-  STREAM_REGISTRATION_RESOURCE=https://dataflow.spring.io/rabbitmq-kafka-latest
+  STREAM_REGISTRATION_RESOURCE=https://dataflow.spring.io/kafka-maven-latest
 fi
 if [[ $TASK_APPS_VERSION == *"latest"* ]]; then
   TASK_REGISTRATION_RESOURCE=https://dataflow.spring.io/task-kafka-latest
