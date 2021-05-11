cd1f4afd5412febdd802b726ab2cb3a847258d74
David Turanski <dturanski@vmware.com>
2021-05-11 15:04:10 -0400

    Fix schedulesEnabled false

--- cloudfoundry-tile/server/config_service_instance.py
+++ cloudfoundry-tile/server/config_service_instance.py
@@ -3,7 +3,7 @@ import sys
 import os
 
 schedules_enabled = os.getenv(
-    "SPRING_CLOUD_DATAFLOW_FEATURES_SCHEDULES_ENABLED", False)
+    "SPRING_CLOUD_DATAFLOW_FEATURES_SCHEDULES_ENABLED", 'false')
 dataflow_tile_configuration = os.getenv("DATAFLOW_TILE_CONFIGURATION", "")
 config={}
 if len(dataflow_tile_configuration):
@@ -11,7 +11,7 @@ if len(dataflow_tile_configuration):
 
 config['maven-cache']=True
 
-if schedules_enabled and not 'scheduler' in config.keys():
+if schedules_enabled.lower()=='true' and not 'scheduler' in config.keys():
     schedules_service_name = os.environ['SCHEDULES_SERVICE_NAME']
     schedules_plan_name = os.environ['SCHEDULES_PLAN_NAME']
     config['scheduler'] = {'name': schedules_service_name, 'plan': schedules_plan_name}
