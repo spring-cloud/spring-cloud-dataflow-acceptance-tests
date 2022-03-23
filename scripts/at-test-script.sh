399bef14358c452a211e298543086eb4ea9a7cd7
David Turanski <dturanski@vmware.com>
2022-03-23 10:00:25 -0400

    Fix script

--- scripts/at-test-script.sh
+++ scripts/at-test-script.sh
@@ -25,7 +25,8 @@ echo RUNNING TESTS
 # ensure no false prometheus is detected.
 export TEST_PLATFORM_CONNECTION_PROMETHEUS_URL=none
 export MAVEN_PROPERTIES="-Dtest.docker.compose.disable.extension=true -Djavax.net.ssl.trustStore=${PWD}/mycacerts -Djavax.net.ssl.trustStorePassword=changeit"
-status=./run.sh tests -c -cc -se --tests !DataFlowAT#streamAppCrossVersion,!DataFlowAT#streamPartitioning,!BatchRemotePartitioningAT#runBatchRemotePartitionJobCloudFoundry
+./run.sh tests -c -cc -se --tests !DataFlowAT#streamAppCrossVersion,!DataFlowAT#streamPartitioning,!BatchRemotePartitioningAT#runBatchRemotePartitionJobCloudFoundry
+status=$?
 echo FINISHED RUNNING TESTS
 ./run.sh clean -se
 exit $status
