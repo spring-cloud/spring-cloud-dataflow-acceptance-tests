8ff4cfe7265592f0c35ae49cedda8a59430c121c
David Turanski <dturanski@vmware.com>
2022-04-27 19:41:52 -0400

    Python build Attempt 1

--- scripts/at-test-script.sh
+++ scripts/at-test-script.sh
@@ -1,17 +1,43 @@
-#
-# Run AT Tests. Required environment variables must be set.
-#
-#Install required POSTGRESQL lib for python
+#!/bin/bash
+#Install required libs for python
 PATH=$PATH:~/.local/bin
-python3 -m pip install --upgrade pip
-pip3 install psycopg2-binary
-#Install required ORACLE lib for python
-pip3 install cx_Oracle --upgrade --user
-#Install cf cli
+python3 -m pip install --upgrade pip | grep -v 'Requirement already satisfied'
+pip3 install -r requirements.txt | grep -v 'Requirement already satisfied'
+
+load_file() {
+  filename=$1
+  echo "exporting required env variables from $filename :"
+  while IFS='=' read -r var value; do
+  if ! [[ $var == \#* ]]; then
+    # only the un-set variables are exported
+    if [ -z ${!var} ]; then
+      export $var="$(eval echo $value)"
+    fi
+  fi
+  done < "$filename"
+}
+
+
+ARGS=$@
+#This consumes $@ so save to ARGS first
+while [[ $# > 0 && -z $SQL_PROVIDER ]]
+do
+  key="$1"
+  if [[ $key = "--sqlProvider" ]]; then
+    SQL_PROVIDER="$2"
+  fi
+  shift
+done
+
+if [[ ! -z "$SQL_PROVIDER" ]]; then
+  echo "SQL_PROVIDER = $SQL_PROVIDER"
+fi
+
 os=$(uname)
 if [[ "$os" == "Linux" ]]; then
     if ! command -v cf &> /dev/null
     then
+      echo "Installing CloudFoundry CLI"
       wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | sudo apt-key add -
       echo "deb https://packages.cloudfoundry.org/debian stable main" | sudo tee /etc/apt/sources.list.d/cloudfoundry-cli.list
       sudo apt-get update
@@ -19,29 +45,30 @@ if [[ "$os" == "Linux" ]]; then
     fi
     if [[ "$SQL_PROVIDER" == "oracle" ]]; then
       echo "Installing ORACLE components"
-       wget -q https://download.oracle.com/otn_software/linux/instantclient/215000/instantclient-basiclite-linux.x64-21.5.0.0.0dbru.zip
-       unzip instantclient-basiclite-linux.x64-21.5.0.0.0dbru.zip
-       export LD_LIBRARY_PATH=./instantclient_21_5
+      wget -q https://download.oracle.com/otn_software/linux/instantclient/215000/instantclient-basiclite-linux.x64-21.5.0.0.0dbru.zip
+      unzip instantclient-basiclite-linux.x64-21.5.0.0.0dbru.zip
+      export LD_LIBRARY_PATH=$PWD/instantclient_21_5
     fi
+elif [[ "$os" == "Darwin" ]]; then
+  if [[ "$SQL_PROVIDER" == "oracle" ]] ; then
+    if [[ ! -d "./instantclient_19_8" ]]; then
+      echo "Installing ORACLE components"
+      wget -q https://download.oracle.com/otn_software/mac/instantclient/198000/instantclient-basiclite-macos.x64-19.8.0.0.0dbru.zip
+      unzip instantclient-basiclite-macos.x64-19.8.0.0.0dbru.zip
+    fi
+    export LD_LIBRARY_PATH=$PWD/instantclient_19_8
+  fi
+
+fi
+export PYTHONPATH=./src:$PYTHONPATH
+python3 -m install.clean
+if [[ $? > 0 ]]; then
+  exit 1
 fi
 
-SCHED_FLAG=""
-if [ "$SCHEDULER" == "true" ]; then
-        SCHED_FLAG="-se"
+python3 -m install.setup $ARGS
+if [[ $? > 0 ]]; then
+  exit 1
 fi
-
-echo CLEANING UP RESOURCES BEFORE RUNNING TESTS
-./run.sh clean -se
-echo FINISHED CLEANING UP RESOURCES
-# setup environment
-./run.sh setup -cc $SCHED_FLAG
-# Run the tests
-echo RUNNING TESTS
-# ensure no false prometheus is detected.
-export TEST_PLATFORM_CONNECTION_PROMETHEUS_URL=none
-export MAVEN_PROPERTIES="-Dtest.docker.compose.disable.extension=true -Djavax.net.ssl.trustStore=${PWD}/mycacerts -Djavax.net.ssl.trustStorePassword=changeit"
-./run.sh tests -c -cc $SCHED_FLAG --tests !DataFlowAT#streamAppCrossVersion,!DataFlowAT#streamPartitioning,!BatchRemotePartitioningAT#runBatchRemotePartitionJobCloudFoundry
-status=$?
-echo FINISHED RUNNING TESTS
-./run.sh clean $SCHED_FLAG
-exit $status
+load_file "cf_scdf.properties"
+echo "SERVER_URI=$SERVER_URI"
