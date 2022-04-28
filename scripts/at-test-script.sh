dc01e037634037c54b61205a446354a8f3b78dee
David Turanski <dturanski@vmware.com>
2022-04-28 13:42:35 -0400

    Python build Attempt 24

--- scripts/at-test-script.sh
+++ scripts/at-test-script.sh
@@ -27,8 +27,7 @@ function run_tests() {
   export TEST_PLATFORM_CONNECTION_PROMETHEUS_URL=none
   JAVA_TRUST_STORE=${PWD}/scdf_cf_setup/mycacerts
   MAVEN_PROPERTIES="-Dtest.docker.compose.disable.extension=true -Djavax.net.ssl.trustStore=${JAVA_TRUST_STORE} -Djavax.net.ssl.trustStorePassword=changeit"
-#  TESTS="!DataFlowAT#streamAppCrossVersion,!DataFlowAT#streamPartitioning,!BatchRemotePartitioningAT#runBatchRemotePartitionJobCloudFoundry"
-  TESTS="DataFlowAT#timestampTask"
+  TESTS="!DataFlowAT#streamAppCrossVersion,!DataFlowAT#streamPartitioning,!BatchRemotePartitioningAT#runBatchRemotePartitionJobCloudFoundry"
   HTTPS_ENABLED="true"
   SERVER_URI="$SPRING_CLOUD_DATAFLOW_CLIENT_SERVER_URI"
   SKIP_SSL_VALIDATION="$SPRING_CLOUD_STREAM_DEPLOYER_CLOUDFOUNDRY_SKIP_SSL_VALIDATION"
