f11914ccd8f9e9e5fbc426c9e4bc6b343e1897e6
Corneil du Plessis <pcorneil@vmware.com>
2022-05-05 13:56:02 +0200

    Ensure using the jdbc drivers from Maven Central. Invoke build-timestamp-batch-with-drivers.sh before tests.

--- acceptance-tests/ci/scripts/build-timestamp-batch-with-drivers.sh
+++ acceptance-tests/ci/scripts/build-timestamp-batch-with-drivers.sh
@@ -7,7 +7,7 @@ repository=$(pwd)/distribution-repository
 pushd git-repo > /dev/null
 pushd $BASE_PATH > /dev/null
 pushd custom-apps/$APP_TEMPLATE > /dev/null
-./gradlew clean build install -Dmaven.repo.local=${repository} -PprojectBuildVersion=$TASK_APP_VERSION -PspringCloudTaskAppVersion=$TASK_APP_VERSION -PjarPostfix=$APP_VERSION
+./gradlew clean build install -Dmaven.repo.local=${repository}
 popd > /dev/null
 popd > /dev/null
 popd > /dev/null
