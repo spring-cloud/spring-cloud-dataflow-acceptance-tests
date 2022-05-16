5b4ba07f1e48997680e47d00dad8dfa87188fb78
Corneil du Plessis <pcorneil@vmware.com>
2022-05-16 14:44:10 +0200

    Change flag to prefer loading demo task app metadata. (#367)
    * Change flag to prefer loading demo task app metadata.

--- scripts/at-test-script.sh
+++ scripts/at-test-script.sh
@@ -5,10 +5,11 @@ SETUP_TOOL_REPO="scdf_cf_setup"
 echo "SETUP_TOOL_REPO=$SETUP_TOOL_REPO"
 echo "SQL_DATAFLOW_DB_NAME=$SQL_DATAFLOW_DB_NAME"
 echo "SQL_SKIPPER_DB_NAME=$SQL_SKIPPER_DB_NAME"
-#if [[ -d $SETUP_TOOL_REPO ]]; then
-#  rm -rf $SETUP_TOOL_REPO
-#fi
-# git clone https://github.com/dturanski/scdf_cf_setup.git
+if [[ ! -d $SETUP_TOOL_REPO ]]; then
+  echo "Expected $SETUP_TOOL_REPO"
+  exit 2
+fi
+
 python3 -m pip install --upgrade pip | grep -v 'Requirement already satisfied'
 pip3 install -r $SETUP_TOOL_REPO/requirements.txt | grep -v 'Requirement already satisfied'
 
