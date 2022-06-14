6d5fe6301cc96b8dede93f21754d71cd5907d601
Corneil du Plessis <pcorneil@vmware.com>
2022-06-14 22:08:56 +0200

    Improve local development scripts and documentation. (#384)

--- src/local/configure-k8s.sh
+++ src/local/configure-k8s.sh
@@ -30,7 +30,8 @@ case "$K8S_DRIVER" in
   *)
     echo "Creating Minikube cluster with $K8S_DRIVER"
     # K8S_DRIVER=kvm2, docker, vmware, virtualbox, podman, vmwarefusion
-    minikube start --cpus=4 --memory=10g --driver=$K8S_DRIVER
+    minikube start --cpus=6 --memory=10g --driver=$K8S_DRIVER
+    echo "Please run 'minikube tunnel' in a separate shell to ensure a LoadBalancer is active."
   ;;
 esac
 kubectl get namespaces | grep default
@@ -42,9 +43,4 @@ fi
 if [ "$K8S_DRIVER" == "kind" ]
 then
   sh "$SCDIR/k8s/setup-metallb.sh"
-else
-  if [ "$K8S_DRIVER" != "tmc" ]
-  then
-    echo "When using Minikube there is no LoadBalancer which means http sink won't work"
-  fi
 fi
--- src/local/launch-scdf.sh
+++ src/local/launch-scdf.sh
@@ -10,7 +10,10 @@ then
   K8S_DRIVER=kind
 fi
 export PLATFORM_TYPE=kubernetes
-kubectl apply -f "$SCDIR/k8s/metallb-configmap.yaml"
+if [ "$K8S_DRIVER" == "kind" ]
+then
+  kubectl apply -f "$SCDIR/k8s/metallb-configmap.yaml"
+fi
 sh "$SCDIR/k8s/deploy-scdf.sh"
 sh "$SCDIR/load-images.sh"
 echo "Waiting for mariadb"
