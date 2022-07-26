f4e0c30ec7dff464df65095b9f258757406e01f2
Corneil du Plessis <pcorneil@vmware.com>
2022-07-26 17:10:38 +0200

    ensure test uses failsafe. (#403)

--- src/local/launch-scdf.sh
+++ src/local/launch-scdf.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
-(return 0 2>/dev/null) && sourced=1 || sourced=0
-if [ "$sourced" == "0" ]; then
-  echo "This script must be invoked using: source $0 $*"
-  exit 1
-fi
 if [ "$NS" == "" ]; then
   echo "NS not defined" >&2
-  return 2
+  exit 2
 fi
 start_time=$(date +%s)
 SCDIR=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
@@ -55,4 +50,5 @@ sh "$LS_DIR/register-apps.sh"
 end_time=$(date +%s)
 elapsed=$(( end_time - start_time ))
 echo "Complete deployment in $elapsed seconds"
+echo "Execute source $LS_DIR/export-dataflow-ip.sh to export DATAFLOW_IP for executing tests"
 echo "Monitor pods using k9s and kail --ns=default | tee pods.log"
