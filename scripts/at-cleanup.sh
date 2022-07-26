8444955b1fd46ea87d7f22fc32d0123912b2d058
Corneil du Plessis <pcorneil@vmware.com>
2022-07-26 22:53:33 +0200

    improve cleanup

--- scripts/at-cleanup.sh
+++ scripts/at-cleanup.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 pushd $SETUP_TOOL_REPO  > /dev/null
-    echo "PYTHONPATH=$$PYTHONPATH"
-    echo "PWD=$PWD"
-    # If tests fail, clean up anyway.
+    echo "PYTHONPATH=$PYTHONPATH"
+    echo "Cleanup CF environment"
     python3 -m install.clean -v
 popd > /dev/null
