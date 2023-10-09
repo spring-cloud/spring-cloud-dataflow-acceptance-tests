797f8168134f2450fe6cfda1d7850216467b0b37
Corneil du Plessis <pcorneil@vmware.com>
2023-10-09 20:34:25 +0200

    [CI] Update at-test-script.sh to provide for individual tests

--- scripts/at-test-script.sh
+++ scripts/at-test-script.sh
@@ -2,7 +2,15 @@
 export TEST_PLATFORM_CONNECTION_PROMETHEUS_URL=none
 JAVA_TRUST_STORE=${PWD}/scdf_cf_setup/mycacerts
 MAVEN_PROPERTIES="-Dtest.docker.compose.disable.extension=true -Djavax.net.ssl.trustStore=${JAVA_TRUST_STORE} -Djavax.net.ssl.trustStorePassword=changeit"
-TESTS="!DataFlowAT#streamAppCrossVersion,!DataFlowAT#streamPartitioning,!BatchRemotePartitioningAT#runBatchRemotePartitionJobCloudFoundry"
+TESTS_ARG="!DataFlowAT#streamAppCrossVersion,!DataFlowAT#streamPartitioning,!BatchRemotePartitioningAT#runBatchRemotePartitionJobCloudFoundry"
+GROUP_ARG=
+if [ "$TESTS" != "" ]; then
+  echo "Adding $TESTS"
+  TESTS_ARG="$TESTS_ARG,$TESTS"
+else
+  GROUP_ARG="-Dmaven-failsafe-plugin.groups=all,smoke"
+fi
+
 HTTPS_ENABLED="true"
 SERVER_URI="$SPRING_CLOUD_DATAFLOW_CLIENT_SERVER_URI"
 SKIP_SSL_VALIDATION="$SPRING_CLOUD_STREAM_DEPLOYER_CLOUDFOUNDRY_SKIP_SSL_VALIDATION"
@@ -14,8 +22,7 @@ set +e
 ./mvnw -U -B -Dspring.profiles.active=blah -Dtest=$TESTS -DPLATFORM_TYPE=cloudfoundry \
   -DSKIP_CLOUD_CONFIG=true -Dtest.docker.compose.disable.extension=true -Dspring.cloud.dataflow.client.serverUri=$SERVER_URI \
   -Dspring.cloud.dataflow.client.skipSslValidation=$SKIP_SSL_VALIDATION -Dtest.platform.connection.platformName=default \
-  -Dtest.platform.connection.applicationOverHttps=$HTTPS_ENABLED \
-  -Dmaven-failsafe-plugin.groups=all,smoke \
+  -Dtest.platform.connection.applicationOverHttps=$HTTPS_ENABLED $GROUP_ARG \
   $MAVEN_PROPERTIES clean verify surefire-report:failsafe-report-only | tee test-output.log
 # tee masks the mvn output
 RC=$(grep -c -F "BUILD FAILURE" test-output.log)
