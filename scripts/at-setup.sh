ba6ff16cf865edaa9b947abc6a9cbaa492a3dc95
Chris Bono <chris.bono@gmail.com>
2024-09-11 09:34:16 -0500

    Fix for cf-cli cert expired
    Adds a workaround to deal w/ the fact that the
    cf-cli cert is expired.
    
    See https://github.com/cloudfoundry/cli/issues/3194

--- scripts/at-setup.sh
+++ scripts/at-setup.sh
@@ -79,7 +79,7 @@ if [[ "$os" = "Linux" ]]; then
     wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | sudo apt-key add -
     echo "deb https://packages.cloudfoundry.org/debian stable main" | sudo tee /etc/apt/sources.list.d/cloudfoundry-cli.list
     sudo apt-get update
-    sudo apt-get install cf-cli
+    sudo apt-get install cf-cli --allow-unauthenticated -y
   fi
   if [[ "$SQL_PROVIDER" = "oracle" ]]; then
     echo "Installing ORACLE components"
